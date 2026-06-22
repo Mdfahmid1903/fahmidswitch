2446b914dff047ed9c88a73ef03add73d3770edb
fahmid <fahmidhrm@gmail.com>
2026-06-22 20:52:32 +0600

    light

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const button = document.querySelector(".switch");
-const body = document.querySelector("body");
 const button2 = document.querySelector(".switch2");
+const button3 = document.querySelector(".switch3");
+const body = document.querySelector("body");
 const container = document.querySelector(".container");
+const red = document.querySelector(".red");
+const yellow = document.querySelector(".yellow");
+const green = document.querySelector(".green");
 
 button.addEventListener('click', ()=>{
 body.classList.toggle('darkmode');
@@ -15,5 +19,27 @@ if(body.classList.contains('darkmode')){
 
 button2.addEventListener('click', ()=>{
     container.classList.toggle('fan');
+})
+
+button3.addEventListener('click', ()=>{
+    setTimeout(() => {
+        red.classList.toggle('light');
+    }, 1000);
+    
+    setTimeout(() => {
+        red.classList.remove('light');
+        yellow.classList.toggle('light');
+    }, 2000);
+    
+    setTimeout(() => {
+        yellow.classList.remove('light');
+        green.classList.toggle('light');
+    }, 3000);
+    
+    setTimeout(() => {
+        green.classList.remove('light');
+        red.classList.toggle('light');
+    }, 4000);
+    
 })
 
